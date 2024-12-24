ecbd5cd28eb78aa7ce1217abc632647fa30a6f99
t-robarge <126084566+t-robarge@users.noreply.github.com>
2024-12-24 13:40:39 -0500

    Update ECGraphicViewImp.h

--- ECGraphicViewImp.h
+++ ECGraphicViewImp.h
@@ -1,17 +1,4 @@
-//
-//  ECGraphicViewImp.h
-//  
-//
-//  Created by Yufeng Wu on 3/2/22.
-//
-
-//***********************************************************
-//          GraphicView version 1.0.0 alpha
-//                  March 2, 2022
-//                  By Yufeng Wu (all rights reserved)
-//    Disclaimer: this code builts on Allegro game engine
-//
-//***********************************************************
+// Allegro Interface
 
 #ifndef ECGraphicViewImp_h
 #define ECGraphicViewImp_h
